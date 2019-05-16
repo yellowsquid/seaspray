bc2e721598116585a19b72bec19bd363927615b0
Greg Brown <greg.brown.00@outlook.com>
2019-05-16 18:42:01 +0100

    Modify current conditions panel.
    Signed-off-by: Greg Brown <greg.brown.00@outlook.com>

--- src/main/java/uk/ac/cam/group2/seaspray/widget/CurrentPanel.java
+++ src/main/java/uk/ac/cam/group2/seaspray/widget/CurrentPanel.java
@@ -15,35 +15,93 @@ import java.awt.*;
 public class CurrentPanel extends JPanel {
     public CurrentPanel(CurrentData currentData) {
         setLayout(new GridBagLayout());
+        setBackground(Color.WHITE);
         GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.BOTH;
-        c.gridy = 0;
+
+        // Global constraints
+        c.ipadx = 16;
+        c.ipady = 16;
+
+        // Current Conditions
         c.gridx = 0;
-        c.weightx = 1;
-        c.weighty = 1;
+        c.gridy = 0;
+        c.weightx = 0.125;
+        c.weighty = 0.125;
+        c.gridwidth = 1;
+        c.anchor = GridBagConstraints.EAST;
+        c.fill = GridBagConstraints.VERTICAL;
+        // FIXME: conditions icon
+        JLabel cond = new JLabel(currentData.getDesc());
+        add(cond, c);
 
-        JLabel tempC = new JLabel(String.valueOf(currentData.getTempC() + "\u00B0" + "C"));
-        add(tempC,c);
-
-        c.gridy = 1;
-        c.gridx = 1;
-        c.weighty = 3;
-        c.weightx = 3;
-        // TODO: Currently gives error
-        add(new WindWidget(currentData.getWind()),c);
-
-        JLabel wave = new JLabel(currentData.getSigWaveHeightFt()+"ft");
+        // Temperature
+        c.gridx = 0;
         c.gridy = 2;
-        c.gridx = 2;
-        c.weightx = 1;
+        c.weightx = 0.125;
+        c.weighty = 0.125;
+        c.gridwidth = 1;
+        c.anchor = GridBagConstraints.EAST;
+        c.fill = GridBagConstraints.VERTICAL;
+        JLabel tempC = new JLabel(String.valueOf(currentData.getTempC() + "\u00B0" + "C"));
+        // TODO: Adjust font size
+        add(tempC, c);
+
+        // Wind
+        c.gridx = 0;
+        c.gridy = 1;
         c.weighty = 1;
+        c.weightx = 1;
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        c.anchor = GridBagConstraints.CENTER;
+        c.fill = GridBagConstraints.BOTH;
+        add(new WindWidget(currentData.getWind()),c);
+        // TODO: Adjust font size
+
+        // Wave height
+        c.gridx = 2;
+        c.gridy = 2;
+        c.weightx = 0.125;
+        c.weighty = 0.125;
+        c.gridwidth = 1;
+        c.anchor = GridBagConstraints.WEST;
+        c.fill = GridBagConstraints.VERTICAL;
+        JLabel wave = new JLabel(currentData.getSigWaveHeightFt()+"ft");
+        // TODO: Adjust font size
         add(wave,c);
 
-        c.gridx = 3;
+        // Waviness Icon
+        // FIXME: shows wrong iconset
+        c.gridx = 1;
+        c.gridy = 2;
+        c.weightx = 1;
+        c.weighty = 0.125;
+        c.gridwidth = 1;
+        c.anchor = GridBagConstraints.EAST;
+        c.fill = GridBagConstraints.VERTICAL;
         add(GetWaveIcon.getIcon(currentData.getWaveDir()),c);
 
-        // TODO: Wave direction?
+        // Tides
+        // FIXME: currently static placeholder text
+        c.gridx = 0;
+        c.gridy = 3;
+        c.weightx = 0;
+        c.weighty = 0.125;
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        c.anchor = GridBagConstraints.CENTER;
+        c.fill = GridBagConstraints.BOTH;
+        JLabel tides = new JLabel("Tides stuff here");
+        add(tides, c);
 
-
+        // Sunrise and sunset
+        // FIXME: currently static placeholder text
+        c.gridx = 0;
+        c.gridy = 4;
+        c.weightx = 0;
+        c.weighty = 0.125;
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        c.anchor = GridBagConstraints.CENTER;
+        c.fill = GridBagConstraints.BOTH;
+        JLabel sunset = new JLabel("Sunset stuff here");
+        add(sunset, c);
     }
 }
