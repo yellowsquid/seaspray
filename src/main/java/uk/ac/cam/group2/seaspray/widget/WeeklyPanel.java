e0e10c7ec5c125531308f05572ab0bdef077d0a0
Douglas Boyle <dab80@cam.ac.uk>
2019-05-16 17:07:02 +0100

    WeeklyPanel filled out. Need to fix layout/formatting

--- src/main/java/uk/ac/cam/group2/seaspray/widget/WeeklyPanel.java
+++ src/main/java/uk/ac/cam/group2/seaspray/widget/WeeklyPanel.java
@@ -1,15 +1,33 @@
 package uk.ac.cam.group2.seaspray.widget;
 
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import uk.ac.cam.group2.seaspray.data.DailyData;
+import uk.ac.cam.group2.seaspray.data.HourlyData;
 
 /**
  * Displays weather for the next week.
  */
 public class WeeklyPanel extends JPanel {
+
     public WeeklyPanel(List<DailyData> dailyDatas) {
+        setLayout(new GridBagLayout());
+        setBorder(BorderFactory.createMatteBorder(1,0,1,0, Color.BLACK));
+        GridBagConstraints c = new GridBagConstraints();
+        c.weightx = 1;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+
+        for (int i = 1; i < dailyDatas.size(); i++) {
+            DailyData data = dailyDatas.get(i);
+            add(new DailyWidget(data), c);
+            c.gridy++;
+        }
     }
 }
