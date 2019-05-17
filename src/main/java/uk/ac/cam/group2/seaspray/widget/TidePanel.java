84d345491bd1ed17054c9dd8646404a77aae6726
Greg Brown <greg.brown.00@outlook.com>
2019-05-17 11:45:36 +0100

    Reformat tide panel.
    Signed-off-by: Greg Brown <greg.brown.00@outlook.com>

--- src/main/java/uk/ac/cam/group2/seaspray/widget/TidePanel.java
+++ src/main/java/uk/ac/cam/group2/seaspray/widget/TidePanel.java
@@ -1,22 +1,23 @@
 package uk.ac.cam.group2.seaspray.widget;
 
+import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.util.*;
+import java.util.List;
 import javax.swing.*;
 import uk.ac.cam.group2.seaspray.data.TideData;
 
 public class TidePanel extends JPanel {
-    public TidePanel(LinkedList<TideData> data) {
+    public TidePanel(List<TideData> data) {
         setLayout(new GridBagLayout());
+        setBackground(Color.WHITE);
         GridBagConstraints c = new GridBagConstraints();
 
         // Global constraints
         c.gridx = 0;
         c.gridy = 0;
         c.weightx = 1;
-        c.fill = GridBagConstraints.VERTICAL;
-        c.anchor = GridBagConstraints.CENTER;
+        c.fill = GridBagConstraints.NONE;
 
         if (data.size() < 4) {
             JLabel message = new JLabel("Data not available");
@@ -30,10 +31,12 @@ public class TidePanel extends JPanel {
 
                 c.gridy = 0;
                 c.weighty = 0.5;
+                c.anchor = GridBagConstraints.SOUTH;
                 add(label, c);
 
                 c.gridy = 1;
                 c.weighty = 1;
+                c.anchor = GridBagConstraints.NORTH;
                 add(entry, c);
 
                 c.gridx++;
